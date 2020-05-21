8af1df04ab3534b1ed026fbb9febf53c563fb397
OscarXu <15222875592@163.com>
2020-05-21 18:51:35 -0400

    seperate the login infos from the aciton.loginAction.java by creating entity.User.java

--- src/action/loginAction.java
+++ src/action/loginAction.java
@@ -1,34 +1,28 @@
 package action;
 
+import entity.User;
+
 public class loginAction {
 
-        private String username;
-        private String password;
-        private String msg = "login error!";
+    private User user;
 
-        public String execute() throws Exception {
-            System.out.println("username = "+username+"password = "+password);
-            if (username.equals("admin") && password.equals("123")) {
+    private String msg = "login error!";
+
+    public String execute() throws Exception {
+        System.out.println("username = "+user.getUsername()+"password = "+user.getPassword());
+        if (user.getUsername().equals("admin") && user.getPassword().equals("123")) {
                 msg = "login success!";
-            }
-            return "success";
         }
+        return "success";
+    }
 
-        public String getUsername() {
-            return username;
-        }
+    public User getUser() {
+        return user;
+    }
 
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public String getMsg() {
         return msg;
